0df253580191fa256ab090ca9a8bb2959fb1754a
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-06-26 14:39:20 -0300

    feat: Dicts can be used to emulate switch/case statements

---
+++ dict-emulate-switch-case.py
@@ -0,0 +1,36 @@
+# Because Python has first-class functions they can
+# be used to emulate switch/case statements
+
+def dispatch_if(operator, x, y):
+    if operator == 'add':
+        return x + y
+    elif operator == 'sub':
+        return x - y
+    elif operator == 'mul':
+        return x * y
+    elif operator == 'div':
+        return x / y
+    else:
+        return None
+
+
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+print(dispatch_if('mul', 2, 8))
+# output: 16
+
+print(dispatch_dict('mul', 2, 8))
+# output: 16
+
+print(dispatch_if('unknown', 2, 8))
+# output: None
+
+print(dispatch_dict('unknown', 2, 8))
+# output: None
