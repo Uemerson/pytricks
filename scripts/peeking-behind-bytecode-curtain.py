9de3ba585a0b9dc25c07c3cc971715be4469ff0c
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-08-07 15:27:46 -0300

    feat: Peeking Behind The Bytecode Curtain

---
+++ scripts/peeking-behind-bytecode-curtain.py
@@ -0,0 +1,24 @@
+# You can use Python's built-in "dis"
+# module to disassemble functions and
+# inspect their CPython VM bytecode:
+
+import dis
+
+
+def greet(name):
+    return 'Hello, ' + name + '!'
+
+
+greet('Dan')
+# output:
+# 'Hello, Dan!'
+
+dis.dis(greet)
+
+# output:
+# 2   0 LOAD_CONST     1 ('Hello, ')
+#     2 LOAD_FAST      0 (name)
+#     4 BINARY_ADD
+#     6 LOAD_CONST     2 ('!')
+#     8 BINARY_ADD
+#    10 RETURN_VALUE
