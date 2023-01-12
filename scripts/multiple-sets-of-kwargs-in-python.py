f8ff6074f044c0fbf5b520980fbb298f9741d5c4
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2023-01-11 22:51:42 -0300

    feat: Multiple sets of kwargs in Python 3.5+

---
+++ scripts/multiple-sets-of-kwargs-in-python.py
@@ -0,0 +1,18 @@
+# Python 3.5+ allows passing multiple sets
+# of keyword arguments ("kwargs") to a
+# function within a single call, using
+# the "**" syntax:
+
+
+def process_data(a, b, c, d):
+    print(a, b, c, d)
+
+
+x = {"a": 1, "b": 2}
+y = {"c": 3, "d": 4}
+
+process_data(**x, **y)
+# output: 1 2 3 4
+
+process_data(**x, c=23, d=42)
+# output: 1 2 23 42
